e7f02de4cfc8ff8bb8dc0366be78d1fff8f71bb4
Alexey Taran <41824639+alextar04@users.noreply.github.com>
2019-02-03 12:43:43 +0300

    Update GaussSlau.h
    fixed bugs

--- GaussSlau.h
+++ GaussSlau.h
@@ -2,17 +2,17 @@
 #define RAZIONALSLAU_GAUSSSLAU_H
 #include "input.h"
 #include "OperationsModule.h"
-typedef char num[100];
+typedef char *num;
 
 void output1(fraction **matr, int countStrings, int countColumns);
-void consoleOrTxtInput(int choose, num **matrS, int countStrings, int countColumns);
-void matrStrings(num** matrS,int countStrings, int countColumns);
+int consoleOrTxtInput(int choose, fraction **matrS);
+int matrFractions(fraction** matrS);
 void swapString(fraction **a, fraction **b);
-fraction** matrFractions(num** matrS, int countStrings, int countColumns);
 int compare(fraction a, fraction max);
 int searchColMaxElem(fraction **matr, int k, int countStrings);
 int directGauss(fraction **matr, int countStrings, int countColumns);
 fraction summa(fraction **matr, int k, fraction *x, int countStrings);
+void reverseGaussMuchSolving(fraction **matr, int countString, int countColumns);
 fraction* reverseGauss(fraction **matr, int countStrings, int countColumns);
 void solvingSlau(fraction **matr, int countStrings, int countColumns);
 #endif //RAZIONALSLAU_GAUSSSLAU_H
